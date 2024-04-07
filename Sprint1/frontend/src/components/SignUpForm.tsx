7a3b6eb19f02f4e4569fa651696759de153b4eab
unknown <omar.vite.a@uni.pe>
2024-04-07 13:15:46 -0500

    feat: added SignUp view design

---
+++ Sprint1/frontend/src/components/SignUpForm.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import Button from './Button'
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const handleLogin = () => {}
+
+  return (
+    <div className='w-full flex flex-col gap-10 items-center'>
+      <form className='flex flex-col w-full text-gray-600 gap-3 items-start'>
+        <div className='flex flex-col gap-1 w-full'>
+          <label>Usuario</label>
+          <input
+            type='text'
+            value={username}
+            className='border-2 border-slate-300 h-10 rounded-lg px-4'
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div className='flex flex-col gap-1 w-full'>
+          <label>Correo electrónico</label>
+          <input
+            type='email'
+            value={email}
+            className='border-2 border-slate-300 h-10 rounded-lg px-4'
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='flex flex-col gap-1 w-full'>
+          <label>Contraseña</label>
+          <input
+            type='password'
+            value={password}
+            className='border-2 border-slate-300 h-10 rounded-lg px-4'
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+      </form>
+      <div className='flex flex-col w-full justify-center items-center'>
+        <Button onClick={handleLogin} text='Registrarse' width='w-full' />
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
--- Sprint1/frontend/src/views/LoginPage.tsx
+++ Sprint1/frontend/src/views/LoginPage.tsx
@@ -1,6 +1,12 @@
-import LoginForm from '../components/LoginForm'
+import { useState } from 'react'
+import SignInForm from '../components/SignInForm'
+import SignUpForm from '../components/SignUpForm'
 
 const LoginPage = () => {
+  const [view, setView] = useState(true)
+  const handleView = () => {
+    setView(!view)
+  }
   return (
     <main className='flex h-full justify-center items-center'>
       <section className='h-[80%] w-[55%] flex rounded-xl shadow-xl overflow-hidden'>
@@ -10,8 +16,19 @@ const LoginPage = () => {
             className='object-cover w-full'
           />
         </div>
-        <div className='flex bg-stone-50 w-1/2 py-24 px-20'>
-          <LoginForm />
+        <div className='flex flex-col justify-evenly bg-stone-50 w-1/2 py-24 px-20'>
+          <h2 className='flex gap-3 text-4xl justify-center items-center font-bold'>
+            Juego de <span className='text-blue-500'> Damas</span>
+          </h2>
+          <div className='flex flex-col w-full justify-center items-center gap-5'>
+            {view ? <SignInForm /> : <SignUpForm />}
+            <div className='flex w-full justify-center text-sm text-gray-500'>
+              {view ? 'Eres nuevo?' : 'Ya tienes cuenta?'}
+              <button onClick={handleView} className='ml-1 text-blue-400'>
+                {view ? 'Registrate' : 'Inicia Sesión'}
+              </button>
+            </div>
+          </div>
         </div>
       </section>
     </main>
